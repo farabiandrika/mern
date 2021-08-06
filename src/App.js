0595305dd45535552062cf779f7199a9338ac7cf
farabiandrika <38185143+farabiandrika@users.noreply.github.com>
2021-08-07 03:46:14 +0700

    Input Number

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import "assets/scss/style.scss";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import LandingPage from "elements/pages/LandingPage";
+import Examples from "elements/pages/Examples";
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Route path="/" component={LandingPage}></Route>
+        <Route exact path="/" component={LandingPage}></Route>
+        <Route exact path="/examples" component={Examples}></Route>
       </Router>
     </div>
   );
